85a16e3dc8971ccf60de6aa4da4332d6af92b0ba
AlonsoMarreroBello <usr3891@salesianos-lacuesta.net>
2025-03-13 10:17:45 +0000

    first commit

---
+++ src/main/java/com/osirix/api/OsirixApiApplication.java
@@ -0,0 +1,13 @@
+package com.osirix.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsirixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsirixApiApplication.class, args);
+	}
+
+}
