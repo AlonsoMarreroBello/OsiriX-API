272a4c064977fbd4539dc6f63109cdf50073e516
AlonsoMarreroBello <usr3891@salesianos-lacuesta.net>
2025-04-23 18:17:25 +0100

    feat(): Created NotificationService for notification operations and NotificationResponseDto.

---
+++ src/main/java/com/osirix/api/service/NotificationService.java
@@ -0,0 +1,18 @@
+package com.osirix.api.service;
+
+import java.util.List;
+
+import com.osirix.api.dto.NotificationResponseDto;
+
+public interface NotificationService {
+	
+	NotificationResponseDto getById(Long id);
+	List<NotificationResponseDto> getNotificationsByUserId(Long userId);
+	
+	NotificationResponseDto sendNotification(Long userId, String message);
+	NotificationResponseDto markNotificationAsSeen(Long notificationId);
+	
+	void deleteNotification(Long id);
+	
+
+}
