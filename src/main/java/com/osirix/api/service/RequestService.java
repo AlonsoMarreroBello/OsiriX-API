2dd3473563302ec94f78cd7e7df5ca8ccddca63b
AlonsoMarreroBello <usr3891@salesianos-lacuesta.net>
2025-05-03 13:58:55 +0100

    feat(RequestService): Created service interface for Requests.

---
+++ src/main/java/com/osirix/api/service/RequestService.java
@@ -0,0 +1,29 @@
+package com.osirix.api.service;
+
+import java.util.List;
+
+import com.osirix.api.dto.request.RequestDto;
+import com.osirix.api.dto.request.RequestResponseDto;
+import com.osirix.api.dto.request.RequestSimpleDto;
+
+public interface RequestService {
+	
+	List<RequestResponseDto> getAll();
+	
+	RequestResponseDto getById(Long requestId);
+	
+	List<RequestResponseDto> getByUserId(Long userId);
+	
+	RequestResponseDto create(RequestSimpleDto request);
+	
+	RequestResponseDto update(Long requestId, RequestDto request);
+	
+	void delete(Long requestId);
+	
+	
+	
+	// PublicationRequest
+	
+	List<RequestResponseDto> getByAsignedStaffId(Long staffId);
+
+}
