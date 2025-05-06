6c7c11e3a46827e4bfaa33f2443a29a80d88d901
AlonsoMarreroBello <usr3891@salesianos-lacuesta.net>
2025-05-06 14:46:44 +0100

    feat(): implemented logic to StaffService and methods to RoleRepository

--- src/main/java/com/osirix/api/repository/RoleRepository.java
+++ src/main/java/com/osirix/api/repository/RoleRepository.java
@@ -1,5 +1,9 @@
 package com.osirix.api.repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +11,9 @@ import com.osirix.api.entity.Role;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+	
+	Optional<Role> findRoleByName(String name);
+
+	Set<Role> findByNameIn(List<String> roleNames);
 
 }
